1c78d5a59b29e74ff114c21e54a4b261ab277999
Lena Viktorsson <lena.viktorsson@smhi.se>
2024-09-17 10:22:50 +0200

    bug fix return nan
    issue #5 return nan when parameter not in statistic file
    also ruff formating

--- src/nodc_statistics/calculate_parameter.py
+++ src/nodc_statistics/calculate_parameter.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 
-def get_prio_par_oxy(BTL, CTD, q_prio1_par='', q_prio2_par=''):
-
-    if not np.isnan(BTL) and q_prio1_par not in ['B']:
+def get_prio_par_oxy(BTL, CTD, q_prio1_par="", q_prio2_par=""):
+    if not np.isnan(BTL) and q_prio1_par not in ["B"]:
         return BTL
-    elif not np.isnan(CTD) and q_prio2_par not in ['B']:
+    elif not np.isnan(CTD) and q_prio2_par not in ["B"]:
         if CTD < 0.2:
             return np.nan
         else:
@@ -14,19 +13,18 @@ def get_prio_par_oxy(BTL, CTD, q_prio1_par='', q_prio2_par=''):
         return np.nan
 
 
-def get_prio_par(prio1_par, prio2_par, q_prio1_par='', q_prio2_par=''):
-
-    if not np.isnan(prio1_par) and q_prio1_par not in ['B']:
+def get_prio_par(prio1_par, prio2_par, q_prio1_par="", q_prio2_par=""):
+    if not np.isnan(prio1_par) and q_prio1_par not in ["B"]:
         return prio1_par
-    elif  q_prio2_par not in ['B']:
+    elif q_prio2_par not in ["B"]:
         return prio2_par
     else:
         return np.nan
 
-def get_allowed_depth_interval(deph, standard_depths):
 
+def get_allowed_depth_interval(deph, standard_depths):
     """
-    Sätt spann för djup nära standarddjupen. 
+    Sätt spann för djup nära standarddjupen.
     Mindre spann i ytan och större spann ju djupare vi kommer
     """
     # TODO: use basin specific standard depths (Skagerrak and Kattegat differs from the Baltic)
@@ -49,17 +47,18 @@ def get_allowed_depth_interval(deph, standard_depths):
         # djupdjupvatten returnera alla mätning +/- 50 m (100 m spann)
         return deph - 49.999, deph + 50
 
+
 def map_to_standard_depth(data, standard_depths: dict):
     """
-    maps sample depths in DEPH column to standard depths as defined by 
+    maps sample depths in DEPH column to standard depths as defined by
     get_allowed_depth_interval
     mapped standard depths are stored in column depth
     """
 
-    data['depth'] = data['DEPH']
+    data["depth"] = data["DEPH"]
     for depth in standard_depths:
         start, stop = get_allowed_depth_interval(depth, standard_depths=standard_depths)
-        boolean = (data['DEPH'] >= start) & (data['DEPH'] <= stop)
-        data.loc[boolean, 'depth'] = depth
+        boolean = (data["DEPH"] >= start) & (data["DEPH"] <= stop)
+        data.loc[boolean, "depth"] = depth
 
     return data
